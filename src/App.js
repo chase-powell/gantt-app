67af3bd220dd1cfce78d3f49cabec462a3c18bdc
JasonCornish5 <jason.cornish5@yahoo.com>
2022-03-22 18:55:43 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Gantt from "./components/Gantt";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = {
+  data: [
+    {
+      id: 1,
+      text: "Task #1",
+      start_date: "2019-04-15",
+      duration: 3,
+      progress: 0.6,
+    },
+    {
+      id: 2,
+      text: "Task #2",
+      start_date: "2019-04-18",
+      duration: 3,
+      progress: 0.4,
+    },
+  ],
+  links: [{ id: 1, source: 1, target: 2, type: "0" }],
+};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="gantt-container">
+          <Gantt tasks={data} />
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
