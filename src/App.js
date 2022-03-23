a7db167e905764bb16618989d4e7341fb6a4b441
pjwhite08 <pamelajeanwhite@gmail.com>
2022-03-23 18:31:02 -0500

    added new Gantt data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React, { Component } from "react";
-import Gantt from "./components/Gantt";
+import { SVGGantt, CanvasGantt, StrGantt } from 'gantt';
 import "./App.css";
 
-const data = {
-  data: [
-    {
-      id: 1,
-      text: "Task #1",
-      start_date: "2019-04-15",
-      duration: 3,
-      progress: 0.6,
-    },
-    {
-      id: 2,
-      text: "Task #2",
-      start_date: "2019-04-18",
-      duration: 3,
-      progress: 0.4,
-    },
-  ],
-  links: [{ id: 1, source: 1, target: 2, type: "0" }],
-};
+const data = [{
+  id: 1,
+  type: 'group',
+  text: '1 Waterfall model',
+  start: new Date('2018-10-10T09:24:24.319Z'),
+  end: new Date('2018-12-12T09:32:51.245Z'),
+  percent: 0.71,
+  links: []
+}, {
+  id: 11,
+  parent: 1,
+  text: '1.1 Requirements',
+  start: new Date('2018-10-21T09:24:24.319Z'),
+  end: new Date('2018-11-22T01:01:08.938Z'),
+  percent: 0.29,
+  links: [{
+    target: 12,
+    type: 'FS'
+  }]
+}, {
+  id: 12,
+  parent: 1,
+  text: '1.2 Design',
+  start: new Date('2018-11-05T09:24:24.319Z'),
+  end: new Date('2018-12-12T09:32:51.245Z'),
+  percent: 0.78,
+}];
+
+new SVGGantt('#svg-root', data, {
+  viewMode: 'week'
+});
+
+new CanvasGantt('#canvas-root', data, {
+  viewMode: 'week'
+});
+
+const strGantt = new StrGantt(data, {
+  viewMode: 'week'
+});
+this.body = strGantt.render();
+
+
 class App extends Component {
   render() {
     return (
