9295a61808cabf90ac563533f45a41d1ff7a04e3
JasonCornish5 <jason.cornish5@yahoo.com>
2022-03-29 20:01:02 -0500

    got custom tasks working

--- src/App.js
+++ src/App.js
@@ -18,6 +18,13 @@ const data = {
       duration: 3,
       progress: 0.4,
     },
+    {
+      id: 3,
+      text: "Task #2",
+      start_date: "2019-04-18",
+      duration: 3,
+      progress: 0.4,
+    },
   ],
   links: [{ id: 1, source: 1, target: 2, type: "0" }],
 };
--- src/components/Gantt/Gantt.js
+++ src/components/Gantt/Gantt.js
@@ -1,43 +1,62 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { gantt } from "dhtmlx-gantt";
+import { AiFillPlusCircle } from "react-icons/ai";
 import "dhtmlx-gantt/codebase/dhtmlxgantt.css";
 import "./Gantt.css";
 
-
-// export default class Gantt extends Component {
-//   componentDidMount() {
-//       gantt.config.date_format = "%Y-%m-%d %H:%i";  
-//       const { tasks } = this.props;
-//       gantt.init(this.ganttContainer);
-//       gantt.parse(tasks);
-//   }
-
-//   render() {
-//      return (
-//          <div
-//               ref={ (input) => { this.ganttContainer = input } }
-//               style={ { width: '100%', height: '100%' } }
-//           ></div>
-//      );
-//   }
-// }
-
-
 const Gantt = (props) => {
-  const { tasks } = props
+  const [columns, setColumns] = useState();
+
+  const { tasks } = props;
   const containerRef = useRef(null);
+
   useEffect(() => {
     gantt.config.date_format = "%Y-%m-%d %H:%i";
     gantt.init(containerRef.current);
     gantt.parse(tasks);
-    console.log(gantt)
   }, []);
 
+  document.addEventListener("DOMContentLoaded", (event) => {
+    gantt.config.columns = [
+      {
+        name: "text",
+        label: "Task Name",
+        width: "150",
+        tree: true,
+        resize: true,
+      },
+      {
+        name: "Start Date",
+        label: "Start Date",
+        align: "center",
+        resize: true,
+      },
+      {
+        name: "End Date",
+        label: "End Date",
+        align: "center",
+        width: 100,
+      },
+      {
+        name: "add",
+        label: "add",
+        align: "center",
+        width: 100,
+      },
+      {
+        name: "Edit",
+        label: "Edit",
+        align: "center",
+        width: 100,
+      },
+    ];
+  });
+
   return (
-    <div
-      ref={containerRef}
-      style={{ width: "100%", height: "100%" }}
-    ></div>
+    <div>
+      {/* <button onClick={setState(1)}></button> a */}
+      <div ref={containerRef} style={{ width: "100%", height: "100%" }}></div>
+    </div>
   );
 };
 
